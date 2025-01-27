845ef1cacdf54f4a41d0e5f8ea82b9bdcf0cb353
Remy Davison <remydavison@remyswokmacbook.mynetworksettings.com>
2025-01-27 12:04:55 -0500

    removed extra img files and added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 const Header = () => {
     return(
-        <header></header>
+        <header>
+            <meta name="description" content="Little Lemon is the #1 Greek restaurant in Chicago! Book a table or order online." />
+            <meta name="og:title" content="Little Lemon Restaurant" />
+            <meta name="og:description" content="Little Lemon is the #1 Greek restaurant in Chicago! Book a table or order online." />
+            <meta name="og:image" content="" />
+        </header>
     )
 }
