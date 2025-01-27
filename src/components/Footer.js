30ec9f13bb7ce3572fe77f2dfd1d0c55764f5aac
Remy Davison <remydavison@remyswokmacbook.mynetworksettings.com>
2025-01-27 12:05:32 -0500

    actually added prev commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,6 +24,24 @@ const Footer = () => {
                     <a href="/login">Login</a>
                 </li>
             </ul>
+
+            <h3>Contact</h3>
+            <p>
+                123 Main Street<br />
+                Chicago, IL 12345
+            </p>
+            <p>
+                <a href="tel:5555555555" aria-label="link to call restaurant telephone number">(555) 555-5555</a>
+            </p>
+            <p>
+                <a href="mailto:example@email.com" aria-label="link to email restaurant">example@email.com</a>
+            </p>
+
+            <h3>Follow Us</h3>
+            <img src="" alt="facebook logo"/>
+            <img src="" alt="instagram logo"/>
+            <img src="" alt="twitter logo"/>
+            <img src="" alt="tiktok logo"/>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
             <meta name="description" content="Little Lemon is the #1 Greek restaurant in Chicago! Book a table or order online." />
             <meta name="og:title" content="Little Lemon Restaurant" />
             <meta name="og:description" content="Little Lemon is the #1 Greek restaurant in Chicago! Book a table or order online." />
-            <meta name="og:image" content="" />
+            <meta name="og:image" content="././public/social-image.jpg" />
         </header>
     )
 }
