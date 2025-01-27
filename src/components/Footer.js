a0f1833622b6573b05a2e05867f0a6fd93160714
Remy Davison <remydavison@remyswokmacbook.mynetworksettings.com>
2025-01-27 12:00:05 -0500

    added footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,29 @@
 const Footer = () => {
     return(
-        <footer></footer>
+        <footer>
+            <img src="" alt="Little Lemon logo"/>
+
+            <h3>Navigation</h3>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/menu">Menu</a>
+                </li>
+                <li>
+                    <a href="/reservations">Reservations</a>
+                </li>
+                <li>
+                    <a href="/order">Order Online</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+            </ul>
+        </footer>
     )
 }
