02a798aa5746cd47ba5382ec60c9a1cad15d190a
Remy Davison <remydavison@remyswokmacbook.mynetworksettings.com>
2025-02-18 10:31:17 -0500

    added reducer function to handle available times list in booking component

--- src/components/RezSelect.js
+++ src/components/RezSelect.js
@@ -8,12 +8,11 @@ function getDate() {
     return `${dayOfWeek}${month}${day}`;
 }
 
-const RezSelect = () => {
+const RezSelect = (props) => {
 
     const [rezDate, setRezDate] = useState(getDate()); //make pretty
     const [rezTime, setRezTime] = useState("12:00");
     const [rezPeople, setRezPeople] = useState("2");
-    const [availableTimes, setAvailableTimes] = useState(["12:00", "12:15", "12:30", "12:45", "1:00", "1:15", "1:30", "1:45", "2:00", "2:15", "2:30", "2:45", "3:00", "3:15", "3:30", "3:45", "4:00", "4:15", "4:30", "4:45", "5:00", "5:15", "5:30", "5:45", "6:00", "6:15", "6:30", "6:45", "7:00", "7:15", "7:30", "7:45", "8:00"]);
 
     function populateTimes(userTime) {
 
@@ -26,13 +25,13 @@ const RezSelect = () => {
 
         //generates a list of the next 12 times
         //ADD ERROR IF PAST 8:00PM
-        const startingIndex = findTime(userTime, availableTimes); //finds index of user inputted time
-        const displayedTimes = [availableTimes[startingIndex]]; //starts at that index in list of available times
+        const startingIndex = findTime(userTime, props.availableTimes); //finds index of user inputted time
+        const displayedTimes = [props.availableTimes[startingIndex]]; //starts at that index in list of available times
         let currentIndex = startingIndex; //resets current index to keep moving along the array
 
         for (let i = 0; (i < 12); i++) {
             currentIndex += 1;
-            displayedTimes.push(availableTimes[currentIndex]);
+            displayedTimes.push(props.availableTimes[currentIndex]);
         }
 
         return displayedTimes; //returns array of times to display
